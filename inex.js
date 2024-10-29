2b95bc00aad1a29cf341207149e6ec88adaecc46
TanhIsMe <ttmtam1986@gmail.com>
2024-10-29 22:15:07 +0700

    Update inex.js

--- inex.js
+++ inex.js
@@ -1,3 +1,9 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>𝐭𝐚𝐧𝐡𝐢𝐬𝐦𝐞𝐞</title>
     <style>
         body {
